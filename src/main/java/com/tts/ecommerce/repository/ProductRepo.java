ddea64ef0985b4f35262daacffabc909a7975951
Karson Krieg <klkrieg3@gmail.com>
2021-03-04 20:57:52 -0600

    uncomment a line

--- src/main/java/com/tts/ecommerce/repository/ProductRepo.java
+++ src/main/java/com/tts/ecommerce/repository/ProductRepo.java
@@ -3,11 +3,11 @@ package com.tts.ecommerce.repository;
 import com.tts.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-//@Repository
+@Repository
 public interface ProductRepo extends CrudRepository<Product, Long> {
 
   List<Product> findAll();
